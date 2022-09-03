b0f55001faca79cb9911f98373dd138d460a2b68
crhuber <crhuber@users.noreply.github.com>
2022-09-03 20:49:35 +0200

    fixes https://github.com/crhuber/golinks/issues/1

--- pkg/controllers/middleware.go
+++ pkg/controllers/middleware.go
@@ -3,13 +3,16 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func (ac *AppController) LogRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logString := fmt.Sprintf("%v %v %v", r.RemoteAddr, r.Method, r.URL)
+		escapedUrl := strings.Replace(r.URL.String(), "\n", "", -1)
+		escapedUrl = strings.Replace(escapedUrl, "\r", "", -1)
+		logString := fmt.Sprintf("%v %v %v", r.RemoteAddr, r.Method, escapedUrl)
 		log.Info(logString)
 		next.ServeHTTP(w, r)
 	})
